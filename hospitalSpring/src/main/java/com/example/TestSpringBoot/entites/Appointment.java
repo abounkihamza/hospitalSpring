db50c0d3bdb3e404041d2c31c8d190b62718f409
MazzouZ <51268094+MazzouZ@users.noreply.github.com>
2021-01-17 01:11:38 +0100

    add appointment class

---
+++ hospitalSpring/src/main/java/com/example/TestSpringBoot/entites/Appointment.java
@@ -0,0 +1,64 @@
+package com.example.TestSpringBoot.entites;
+
+
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Appointment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String number;
+	private String email;
+	private Date startDate;
+	private Date endDate;
+	
+	public Appointment() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	@Override
+	public String toString() {
+		return "Appointment [id=" + id + ", name=" + name + ", number=" + number + ", email=" + email + ", startDate="
+				+ startDate + ", endDate=" + endDate + "]";
+	}
+	
+	
+}
